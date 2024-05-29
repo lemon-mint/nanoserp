41e7ffab32fba34555e7ceeb32a62b9c45440a11
lemon-mint <ice.lemons.mint@gmail.com>
2024-05-29 14:15:06 +0900

    Fix

--- searxng.go
+++ searxng.go
@@ -20,12 +20,11 @@ func SearchSearXNG(client *http.Client, endpoint, keyword string, engines []stri
 	q.Set("engines", strings.Join(engines, ","))
 	u.RawQuery = q.Encode()
 
-	fmt.Println(u.String())
-
 	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
 		return nil, err
 	}
+
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
 	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
